1da3598464b50ff61593f6adc7c1f5fc650bc46d
Neville Omangi <nevomangi@gmail.com>
2019-08-13 02:30:51 +0300

    separate todo data and dom

--- js/todo.js
+++ js/todo.js
@@ -1,26 +1,77 @@
 import { el, appendChildren, _ } from './dom.js'
-import { deleteTodo, updateLength } from './main.js'
+import { updateLength, deleteTodoDOM, updateApp } from './main.js'
 
 let labelCount = 0
 
 class Todo {
-  constructor (text) {
-    this.text = text
-    this.done = false
-    
+  /**
+   *      
+   * @param {object} todoValues
+   * 
+   * @param {string} todoValues.textVal
+   * @param {boolean} todoValues.doneVal
+   * @param {number}  todoValues.created
+   */
+  constructor ({textVal = '', doneVal = false, created = Date.now()}) {
+    this.textVal = textVal
+    this.doneVal = doneVal
+    this.created = created
+
+    this.watchers = []
+  }
+
+  set text(newVal) {
+    this.textVal = newVal
+    this.notify('text', newVal)
+  }
+
+  get text() {
+    return this.textVal
+  }
+
+  set done(newVal) {
+    this.doneVal = newVal
+    this.notify('done', newVal)
+  }
+
+  get done() {
+    return this.doneVal
+  }
+
+  subscribe(fn, context) {
+    this.watchers.push({fn, context})
+  }
+
+  notify(name, newVal) {
+    if (this.watchers.length > 0) 
+      this.watchers.forEach(({context, fn}) => fn.call(context, {name, newVal}))
+  }
+
+  detachWatchers() {
+    this.watchers = []
+  }
+}
+
+class TodoDOM {
+  constructor(todo) {
+    // watch for to updates
+    this.todo = todo
+
+    todo.subscribe(this.update, this)
+        
     this.el = el('li', _ , 'todo')
 
-    this.textEl = el('div', {spellcheck: false,} , 'text', text)
+    this.textEl = el('div', {spellcheck: false,} , 'text', todo.text)
 
     // edit text
-
     this.textEl.addEventListener('dblclick', ({target}) => {
       target.setAttribute('contenteditable', true)
       target.focus()
     })
 
     this.textEl.addEventListener('blur', ({target}) => {
-      this.updateText(target.textContent)
+      todo.text = target.textContent
+      updateApp()
     })
 
     this.textEl.addEventListener('keydown', ({target, keyCode}) => {
@@ -32,22 +83,13 @@ class Todo {
 
     // mark as done/not done
 
-    // terrible hack to style checkboxes
-    labelCount += 1
-
+    labelCount += 1 // terrible hack to style checkboxes
     const checkLabel = el('label', {for: `check-${labelCount}`})
     const doneEl = el('input', {type: 'checkbox', id: `check-${labelCount}`})
 
     doneEl.addEventListener('change', event => {
-      this.el.classList.toggle('done')
-      if (this.done) {
-        this.done = false
-        // this.el.parentNode.prepend(this.el)
-      } else {
-        this.done = true
-        // this.el.parentNode.appendChild(this.el)
-      }
-      updateLength()
+      todo.done = !todo.done
+      updateApp()
     })
 
     const checkboxEl = el('div', _ , ['checkbox', 'filled'], [doneEl, checkLabel])
@@ -56,20 +98,21 @@ class Todo {
     deleteEl.innerHTML = '&times;'
     deleteEl.addEventListener('click', event => {
       // this is Todo
-
-      deleteTodo(this)
+      deleteTodoDOM(this)
     })
 
     appendChildren(this.el, [checkboxEl, this.textEl, deleteEl,])
   }
 
-  updateText(newVal) {
-    this.text = newVal
-    this.textEl.textContent = newVal
-  }
+  update({name, newVal}) {
+    if (name === 'text') {
+      this.textEl.textContent = newVal
+    }
 
-  delete() {
+    if (name === 'done') {
+      this.todo.done === true ? this.el.classList.add('done') : this.el.classList.remove('done')
+    }
   }
 }
 
-export { Todo }
+export { Todo, TodoDOM }
